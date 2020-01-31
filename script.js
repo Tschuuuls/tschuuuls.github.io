2873c147ec006dd4528d6b7060afaaff3df0f346
Tschuuuls <playitlouder@live.de>
2020-01-31 16:08:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  document.addEventListener('DOMContentLoaded', () => {
-  const source = 'https://live-t2.clipmyhorse.tv/live/smil:adaptive.smil/playlist.m3u8?W563lCW79mEvfgunZtDRvxiVgwHde-1ZHeHd5kxzJeP1j68HTyZSBv-i1s5t1DJ0';
+  const source = 'https://live-t2.clipmyhorse.tv/live/smil:adaptive.smil/playlist.m3u8?1Jc8wKIC7UpbQ5NmKyMn85eoKld53Jt1';
   const video = document.querySelector('video');
 
   // For more options see: https://github.com/sampotts/plyr/#options
